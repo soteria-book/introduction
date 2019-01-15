25af53b5c607592aef02ef999beee6044f08ed3a
Mario Gray <mario.gray@gmail.com>
2019-01-15 00:19:11 -0800

    yo

--- introduction/src/main/java/soteria/introduction/IntroductionApplication.java
+++ introduction/src/main/java/soteria/introduction/IntroductionApplication.java
@@ -3,7 +3,7 @@ package soteria.introduction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // <1>
 public class IntroductionApplication {
 
 	public static void main(String[] args) {
